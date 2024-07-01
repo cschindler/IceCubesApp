a85c04a8460c7ab47c1e299a7532a806466a5f5e
Christopher Schindler <schindler.cs@gmail.com>
2024-07-01 15:00:15 +0200

    ScheduledStatuses: add cancel endpoint

--- Packages/Network/Sources/Network/Endpoint/ScheduledStatuses.swift
+++ Packages/Network/Sources/Network/Endpoint/ScheduledStatuses.swift
@@ -3,11 +3,14 @@ import Models
 
 public enum ScheduledStatuses: Endpoint {
   case scheduledStatuses(sinceId: String?)
+  case cancel(id: String)
 
   public func path() -> String {
     switch self {
     case .scheduledStatuses:
       "scheduled_statuses"
+    case let .cancel(id):
+      "scheduled_statuses/\(id)"
     }
   }
 
@@ -16,6 +19,8 @@ public enum ScheduledStatuses: Endpoint {
     case let .scheduledStatuses(sinceId):
       guard let sinceId else { return nil }
       return [.init(name: "max_id", value: sinceId)]
+    case .cancel:
+      return nil
     }
   }
 }
