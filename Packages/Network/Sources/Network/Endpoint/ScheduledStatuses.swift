613f68fff6518717124c0cf1972dd529e5463ef8
Christopher Schindler <schindler.cs@gmail.com>
2024-07-01 15:16:44 +0200

    ScheduledStatuses: add endpoint to update the scheduled date

--- Packages/Network/Sources/Network/Endpoint/ScheduledStatuses.swift
+++ Packages/Network/Sources/Network/Endpoint/ScheduledStatuses.swift
@@ -4,12 +4,15 @@ import Models
 public enum ScheduledStatuses: Endpoint {
   case scheduledStatuses(sinceId: String?)
   case cancel(id: String)
+  case updateScheduledDate(id: String, scheduledAt: ServerDate)
 
   public func path() -> String {
     switch self {
     case .scheduledStatuses:
       "scheduled_statuses"
     case let .cancel(id):
+      "scheduled_statuses/\(id)"
+    case let .updateScheduledDate(id, _):
       "scheduled_statuses/\(id)"
     }
   }
@@ -21,6 +24,8 @@ public enum ScheduledStatuses: Endpoint {
       return [.init(name: "max_id", value: sinceId)]
     case .cancel:
       return nil
+    case let .updateScheduledDate(_, scheduledAt):
+      return [.init(name: "scheduled_at", value: scheduledAt.asDate.ISO8601Format())]
     }
   }
 }
